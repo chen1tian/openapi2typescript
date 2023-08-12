7d32755f5362db8e2b66be6646654f880867107d
yitian.chen <chen1tian@hotmail.com>
2023-08-13 06:36:28 +0800

    修正泛型类没有命名空间的问题

--- test/genSwagger.js
+++ test/genSwagger.js
@@ -3,6 +3,6 @@ const openAPI = require('../dist/index');
 openAPI.generateService({
   schemaPath: 'http://localhost:5000/Zj/swagger/v1/swagger.json',
   serversPath: './temp/servers',  
-  namespace: 'ZjTest',
+  namespace: 'Zj',
   wrapResult: 'ABP.Response'
 });
